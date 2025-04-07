4ef03c8bc52ae9f8fd6c8f32b4bf65d2922453a7
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 22:24:59 -0500

    Update ModaCalculator3.java

--- Actividad4/ModaCalculator3.java
+++ Actividad4/ModaCalculator3.java
@@ -2,6 +2,12 @@ package Actividad4;
 
 public class ModaCalculator3 {
     public static int moda3(int[] a, int prim, int ult) {
+        SetVectors homogeneo = new SetVectors();
+        SetVectors heterogeneo = new SetVectors();
+
+        Limits p = new Limits(a, prim, ult);
+        heterogeneo.insertar(p);
+
         
     }
 }
