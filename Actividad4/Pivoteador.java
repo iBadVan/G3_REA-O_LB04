0bf0b064a09a131aed8eef24e1fbc7798e37d4fb
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 22:23:21 -0500

    Update Pivoteador.java

---
+++ Actividad4/Pivoteador.java
@@ -0,0 +1,22 @@
+package Actividad4;
+
+public class Pivoteador {
+    public static void pivote2(int[] array, int mediana, int prim, int ult, int[] izq, int[] der) {
+        int i = prim, j = prim;
+        int k = ult;
+
+        while (j <= k) {
+            if (array[j] < mediana) {
+                swap(array, i++, j++);
+            } else if (array[j] > mediana) {
+                swap(array, j, k--);
+            } else {
+                j++;
+            }
+        }
+
+        izq[0] = i;
+        der[0] = j;
+    }
+
+}
