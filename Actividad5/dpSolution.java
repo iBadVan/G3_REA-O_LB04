49a10bc2a2efa9de19de71efcb79a8181865bedd
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 22:49:53 -0500

    Update dpSolution.java

---
+++ Actividad5/dpSolution.java
@@ -0,0 +1,15 @@
+package Actividad5;
+
+public class dpSolution {
+    static int getValue(int[] values, int rodLength) {
+        int[] subSolutions = new int[rodLength + 1];
+        for (int i = 1; i <= rodLength; i++) {
+            int tmpMax = -1;
+            for (int j = 0; j < i; j++)
+                tmpMax = Math.max(tmpMax, values[j] + subSolutions[i - j - 1]);
+            subSolutions[i] = tmpMax;
+        }
+        return subSolutions[rodLength];
+    }
+        
+}
