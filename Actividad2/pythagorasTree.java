5967a04e1504f202fb82909e1d3e3cba8f26443f
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 19:40:39 -0500

    Update pythagorasTree.java

--- Actividad2/pythagorasTree.java
+++ Actividad2/pythagorasTree.java
@@ -1,4 +1,6 @@
 package Actividad2;
+import java.awt.*;
+import javax.swing.*;
 
 public class pythagorasTree {
     private int profundidad;
@@ -8,5 +10,14 @@ public class pythagorasTree {
         setPreferredSize(new Dimension(800, 800));
     }
 
-    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(0, 0, getWidth(), getHeight());
+        g2d.setColor(Color.GREEN);
+        // Llamada inicial para la recursión
+        trazaArbol(g2d, 350, 600, 100, -90, profundidad);
+    }
 }
