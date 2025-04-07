e3d87a3883df92325e2221d70ae4c8025fe4cc01
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 19:20:58 -0500

    Update hanoi.java

--- Actividad1/hanoi.java
+++ Actividad1/hanoi.java
@@ -1,5 +1,12 @@
 package Actividad1;
 
 public class hanoi{
-    
+    public static void main(String[] args){
+        torresHanoi(3,1,2,3);
+    }
+
+    //Creando el metodo recursivo para la solucion de las torres hanoi
+    public void torresHanoi(int discos, int torre1, int torre2, int torre3){
+        
+    }
 }
