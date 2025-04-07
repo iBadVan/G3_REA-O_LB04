46dafc82999fb70cfdeb0d8d576458ad976fd567
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 22:08:03 -0500

    Update ModaCalculator2.java

--- Actividad4/ModaCalculator2.java
+++ Actividad4/ModaCalculator2.java
@@ -1,5 +1,14 @@
 package Actividad4;
 
 public class ModaCalculator2 {
+    public static int moda2(int[] array) {
+        int first = 1;
+        int p = 0;
+        int end = array.length - 1;
+        int moda = array[0];
+        int frec = 1;
+        int maxfre = 0;
     
+    
+    }
 }
