75b3ba2958b2647278a2936dd319d35db47c767a
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 22:21:31 -0500

    Update Limits.java

--- Actividad4/Limits.java
+++ Actividad4/Limits.java
@@ -5,5 +5,13 @@ public class Limits  {
     int ult;
     int[] array;
 
-    
+    public Limits(int[] array, int prim, int ult) {
+        this.array = array;
+        this.prim = prim;
+        this.ult = ult;
+    }
+
+    public int length() {
+        return ult - prim + 1;
+    }
 }
