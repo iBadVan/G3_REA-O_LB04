ca2ecac5408cd91d208113b9ebd1e849f7e3ba2f
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 21:57:24 -0500

    Update ModaCalculator.java

--- Actividad4/ModaCalculator.java
+++ Actividad4/ModaCalculator.java
@@ -22,5 +22,13 @@ public class ModaCalculator {
         return moda;
     }
 
-    
+    private static int frecuencia(int[] array, int first, int end, int ele) {
+        if (first > end) return 0;
+        int suma = 0;
+        for (int i = first; i <= end; i++) {
+            if (array[i] == ele)
+                suma++;
+        }
+        return suma;
+    }
 }
