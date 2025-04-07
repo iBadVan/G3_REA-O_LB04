06e8aa7670a996456f0eab695f03e66070c9a2a6
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 19:40:02 -0500

    Update pythagorasTree.java

--- Actividad2/pythagorasTree.java
+++ Actividad2/pythagorasTree.java
@@ -1,5 +1,12 @@
 package Actividad2;
 
 public class pythagorasTree {
+    private int profundidad;
+
+    public pythagorasTree(int profundidad) {
+        this.profundidad = profundidad;
+        setPreferredSize(new Dimension(800, 800));
+    }
+
     
 }
