45aec5e7a07050c096fb9942f1b237ea65eb4e31
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 21:50:06 -0500

    Update contarFrecuencia.java

--- Actividad4/contarFrecuencia.java
+++ Actividad4/contarFrecuencia.java
@@ -1,5 +1,17 @@
 package Actividad4;
 
-public class contarFrecuencia {
+public class contarFrecuencia{
+    private static int mcontarFrecuencia(int[] arreglo, int inicio, int fin, int elemento) {
+        if (inicio > fin) return 0;
+    
+        int contador = 0;
+        for (int i = inicio; i <= fin; i++) {
+            if (arreglo[i] == elemento) {
+                contador++;
+            }
+        }
+    
+        return contador;
+    }
     
 }
