fc66cfbb84631515099ad4231902c3a2ce447243
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-06 21:50:32 -0500

    Update contarFrecuencia.java

--- Actividad4/contarFrecuencia.java
+++ Actividad4/contarFrecuencia.java
@@ -13,5 +13,28 @@ public class contarFrecuencia{
     
         return contador;
     }
+    public static int encontrarModa(int[] arreglo) {
+        int inicio = 0;
+        int fin = arreglo.length - 1;
+    
+        if (inicio == fin) {
+            return arreglo[inicio];
+        }
+    
+        int moda = arreglo[inicio];
+        int frecuenciaMaxima = mcontarFrecuencia(arreglo, inicio, fin, moda);
+    
+        for (int i = inicio + 1; i <= fin; i++) {
+            int frecuenciaActual = mcontarFrecuencia(arreglo, i, fin, arreglo[i]);
+    
+            if (frecuenciaActual > frecuenciaMaxima) {
+                frecuenciaMaxima = frecuenciaActual;
+                moda = arreglo[i];
+            }
+        }
+    
+        return moda;
+    }
+    
     
 }
