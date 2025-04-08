3cdff4d515a5f12f5a91c60a2a6892d87c04c7be
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-07 22:44:25 -0500

    Update SubconjuntoRestringido.java

--- Ejercicio1/SubconjuntoRestringido.java
+++ Ejercicio1/SubconjuntoRestringido.java
@@ -1,5 +1,14 @@
 package Ejercicio1;
 
 public class SubconjuntoRestringido  {
+    public static boolean esPotenciaDe2(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static boolean puedeFormarObjetivo(int[] arr, int target) {
+        int n = arr.length;
+        return backtrack(arr, 0, 0, target);
+    }
+
     
 }
