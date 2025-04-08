6c74654b9674f1f842d3e4173206564027c7b7a0
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-08 00:43:52 -0500

    Update MinCostTravel.java

--- Ejercicio3/MinCostTravel.java
+++ Ejercicio3/MinCostTravel.java
@@ -18,7 +18,27 @@ public class MinCostTravel {
             }
         }
         
+        int[][] C = new int[n][n];
         
+        for (int i = 0; i < n; i++) {
+            C[i][i] = 0; // El coste de ir de un embarcadero a ell mismo es 0
+        }
+        
+        //  dinámica para calcular los costes minimos
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                C[i][j] = Integer.MAX_VALUE; // Inicializamos el coste con un valor grande
+                
+                for (int k = i; k < j; k++) {
+                    // Calcular el coste mínimo
+                    int cost = T[i][k] + C[k][j];
+                    if (cost < C[i][j]) {
+                        C[i][j] = cost;
+                    }
+                }
+            }
+        }
 
     }
 }
