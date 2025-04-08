ccbda7b90462df8139dee0c9375411ef327fd9bb
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-07 22:49:34 -0500

    Update SubconjuntoRestringido.java

--- Ejercicio1/SubconjuntoRestringido.java
+++ Ejercicio1/SubconjuntoRestringido.java
@@ -19,5 +19,33 @@ public class SubconjuntoRestringido  {
         return backtrack(arr, 0, 0, target);
     }
 
+    private static boolean backtrack(int[] arr, int index, int sumaActual, int objetivo) {
+        if (index == arr.length)
+            return sumaActual == objetivo;
+
+        int actual = arr[index];
+
+        //potencia de 2
+        if (esPotenciaDe2(actual)) {
+            return backtrack(arr, index + 1, sumaActual + actual, objetivo);
+        }
+
+        //múltiplos de 5 con restricción
+        if (actual % 5 == 0 && index + 1 < arr.length && arr[index + 1] % 2 != 0) {
+            // No se puede incluir
+            return backtrack(arr, index + 1, sumaActual, objetivo);
+        }
+
+        //incluir el número actual
+        if (backtrack(arr, index + 1, sumaActual + actual, objetivo)) 
+            return true;
+
+        // no incluirlo
+        if (backtrack(arr, index + 1, sumaActual, objetivo)) 
+            return true;
+
+        return false;
+    }
+
 
 }
