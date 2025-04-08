428ee0ead316a1b41614af1965396c5ac5b68a91
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-08 00:42:40 -0500

    Update MinCostTravel.java

--- Ejercicio3/MinCostTravel.java
+++ Ejercicio3/MinCostTravel.java
@@ -1,5 +1,14 @@
 package Ejercicio3;
 
+import java.util.Scanner;
+
 public class MinCostTravel {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Ingrese el número de embarcaderos (n): ");
+        int n = sc.nextInt();
+        
+        int[][] T = new int[n][n];
+    }
 }
