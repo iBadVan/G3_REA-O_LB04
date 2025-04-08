77c41abd942614dc8b2285be1c1b968fbbab1176
iBadVan <ivan.edgardo.reso@gmail.com>
2025-04-08 00:58:33 -0500

    Update MinCostTravel.java

--- Ejercicio3/MinCostTravel.java
+++ Ejercicio3/MinCostTravel.java
@@ -18,39 +18,55 @@ public class MinCostTravel {
             }
         }
         
+        // Usamos un valor suficientemente grande para representar "infinito"
+        int INF = 1000000; // El valor "infinito"
+        
         int[][] C = new int[n][n];
         
+        // Inicialización de la matriz C para los casos donde i == j
         for (int i = 0; i < n; i++) {
-            C[i][i] = 0; // El coste de ir de un embarcadero a ell mismo es 0
+            C[i][i] = 0; // El coste de ir de un embarcadero a sí mismo es 0
         }
         
-        //  dinámica para calcular los costes minimos
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i < n - len + 1; i++) {
-                int j = i + len - 1;
-                C[i][j] = Integer.MAX_VALUE; // Inicializamos el coste con un valor grande
-                
-                for (int k = i; k < j; k++) {
-                    // Calcular el coste mínimo
-                    int cost = T[i][k] + C[k][j];
-                    if (cost < C[i][j]) {
-                        C[i][j] = cost;
+        // Inicializar la matriz C con el valor de "infinito" donde no hay un camino directo
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && T[i][j] == 0) {
+                    C[i][j] = INF; // Representamos con "infinito" donde no hay ruta directa
+                } else if (i != j) {
+                    C[i][j] = T[i][j]; // Asignamos el coste directo donde existe una tarifa
+                }
+            }
+        }
+
+        // Programación dinámica para calcular los costes mínimos
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    // Si hay un camino de i a k y de k a j, actualizamos el coste de i a j
+                    if (C[i][k] != INF && C[k][j] != INF) {
+                        C[i][j] = Math.min(C[i][j], C[i][k] + C[k][j]);
                     }
                 }
             }
         }
 
+        // Imprimir la matriz C (costes mínimos)
         System.out.println("Matriz de costes mínimos C:");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(C[i][j] + " ");
+                if (C[i][j] == INF) {
+                    System.out.print("INF "); // Usamos "INF" para representar los valores no calculados
+                } else {
+                    System.out.print(C[i][j] + " ");
+                }
             }
             System.out.println();
         }
 
+        // Salida: Coste mínimo entre el primer y el último embarcadero
         System.out.println("El coste mínimo para viajar de 0 a " + (n-1) + " es: " + C[0][n-1]);
         
         sc.close();
-
     }
 }
